4bb4e57ad1ccbbe7de0f59fcfe8fd495523f6500
tipsy <pavel.n87@gmail.com>
2021-09-05 01:01:02 +0300

    switched back to plain-text tags

--- dwm/config.h
+++ dwm/config.h
@@ -36,9 +36,11 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
+/*         侀    */
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "侀",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
