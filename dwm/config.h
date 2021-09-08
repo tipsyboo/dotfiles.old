49dd0f83be67fd2e17799b4738c31e5fd6c236f0
tipsy <pavel.n87@gmail.com>
2021-09-08 14:03:54 +0300

    dwmblocks updates

--- dwm/config.h
+++ dwm/config.h
@@ -57,7 +57,7 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
-/*         侀    */
+/*         侀 充   */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
